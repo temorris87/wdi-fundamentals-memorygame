6705bb511849cbef5806a5dfadd17bc9fd2a5017
Tyson Morris <temorris87@yahoo.com>
2019-04-07 17:15:55 -0400

    Finished functions.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,23 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = cards[2];
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert('You found a match!');
+        console.log('You found a match!');
     }
     else {
-        alert('Sorry, try again.');
+        console.log('Sorry, try again.');
     }
 }
+
+var flipCard = function(cardId) {
+    console.log('User flipped ' + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+};
+
+flipCard(0);
+flipCard(2);
