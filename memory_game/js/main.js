219d195fd2310b9912b2ac5623e92360638308dc
Tyson Morris <temorris87@yahoo.com>
2019-04-07 21:05:14 -0400

    Finished DOM section.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -37,14 +37,26 @@ var logCardFlip = function(cardId) {
     console.log('\tPath: ' + cards[cardId].cardImage);
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+    cardId = this.getAttribute('data-id');
     logCardFlip(cardId);
     cardsInPlay.push(cards[cardId].rank);
+
+    this.setAttribute('src', cards[this.getAttribute('data-id')].cardImage);
 
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+
+createBoard();
